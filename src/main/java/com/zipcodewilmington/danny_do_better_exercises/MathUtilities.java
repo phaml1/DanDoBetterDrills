c3f05ec3eef0d5b7d2d841855d257f1b4946721e
Loc <loc@Zipcoders-MacBook-Pro-2.local>
2023-10-31 00:08:27 -0400

    Float add works

--- src/main/java/com/zipcodewilmington/danny_do_better_exercises/MathUtilities.java
+++ src/main/java/com/zipcodewilmington/danny_do_better_exercises/MathUtilities.java
@@ -47,7 +47,7 @@ public class MathUtilities {
      * @return sum of `baseValue` and `difference`
      */
     public Float add(float baseValue, float difference) {
-        return null;
+        return baseValue + difference;
     }
 
     /**
