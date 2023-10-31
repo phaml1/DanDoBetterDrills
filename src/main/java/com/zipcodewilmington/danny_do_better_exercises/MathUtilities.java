d87eccc8a1a2a4afdac8107e6d6b5ece9a204c98
Loc <loc@Zipcoders-MacBook-Pro-2.local>
2023-10-31 00:20:04 -0400

    float multiply works

--- src/main/java/com/zipcodewilmington/danny_do_better_exercises/MathUtilities.java
+++ src/main/java/com/zipcodewilmington/danny_do_better_exercises/MathUtilities.java
@@ -210,7 +210,7 @@ public class MathUtilities {
      * @return product of `multiplicand` by `multiplier`
      */
     public Float multiply(float multiplicand, float multiplier) {
-        return null;
+        return multiplicand * multiplier;
     }
 
     /**
