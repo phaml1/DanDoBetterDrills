88aeac3cf6fbc61a9855165649468622a72b35fa
Loc <loc@Zipcoders-MacBook-Pro-2.local>
2023-10-31 00:12:28 -0400

    double sub  works

--- src/main/java/com/zipcodewilmington/danny_do_better_exercises/MathUtilities.java
+++ src/main/java/com/zipcodewilmington/danny_do_better_exercises/MathUtilities.java
@@ -110,7 +110,7 @@ public class MathUtilities {
      * @return difference between `baseValue` and `difference`
      */
     public Double subtract(double baseValue, double difference) {
-        return null;
+        return baseValue - difference;
     }
 
 
