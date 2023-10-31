cc8751a07f2abf493a6ff41fc6d3e0c6ed20f248
Loc <loc@Zipcoders-MacBook-Pro-2.local>
2023-10-31 00:18:12 -0400

    int multiply works

--- src/main/java/com/zipcodewilmington/danny_do_better_exercises/MathUtilities.java
+++ src/main/java/com/zipcodewilmington/danny_do_better_exercises/MathUtilities.java
@@ -175,7 +175,7 @@ public class MathUtilities {
      * @return product of `multiplicand` by `multiplier`
      */
     public Integer multiply(int multiplicand, int multiplier) {
-        return null;
+        return multiplicand * multiplier;
     }
 
     /**
