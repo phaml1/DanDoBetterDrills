661a67db88a5b3ce94fccb1e5a5a63e458d4fe16
Loc <loc@Zipcoders-MacBook-Pro-2.local>
2023-10-31 00:23:37 -0400

    <= works

--- src/main/java/com/zipcodewilmington/danny_do_better_exercises/PredicateUtilities.java
+++ src/main/java/com/zipcodewilmington/danny_do_better_exercises/PredicateUtilities.java
@@ -37,7 +37,7 @@ public class PredicateUtilities {
      * @return true if `x` is less than or equal to `y`
      */
     public Boolean isLessThanOrEqualTo(int x, int y) {
-        return null;
+        return x <= y;
     }
 
 
