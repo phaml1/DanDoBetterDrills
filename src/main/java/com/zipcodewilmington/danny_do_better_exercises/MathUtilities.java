2bbac39de2b8ad7087346559ab9729b973c6801e
Loc <loc@Zipcoders-MacBook-Pro-2.local>
2023-10-31 00:05:39 -0400

    Long add works

--- src/main/java/com/zipcodewilmington/danny_do_better_exercises/MathUtilities.java
+++ src/main/java/com/zipcodewilmington/danny_do_better_exercises/MathUtilities.java
@@ -20,7 +20,7 @@ public class MathUtilities {
      * @return sum of `baseValue` and `difference`
      */
     public Long add(long baseValue, long difference) {
-        return null;
+        return baseValue + difference;
     }
 
     /**
